70b03d4048f152e9976365f48bb5034e59412421
eibens <l.eibensteiner@gmail.com>
2021-11-16 11:36:21 +0100

    chore: upgrade edcb

--- dev.ts
+++ dev.ts
@@ -1,4 +1,4 @@
-import { cli } from "https://deno.land/x/edcb@v0.9.0-alpha.1/cli.ts";
+import { cli } from "https://deno.land/x/edcb@v1.0.0-alpha.3/cli.ts";
 
 if (import.meta.main) {
   await cli();
